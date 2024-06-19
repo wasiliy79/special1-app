a60e125b18b59ed0cd5a597f589a6d56f8f87d6a
Wasiliy Ovchinnikov <w.ovchinnikov@mail.ru>
2024-06-20 00:13:33 +0700

    укерп

--- js/main.js
+++ js/main.js
@@ -12,10 +12,6 @@ $(function(){
         $('.popup-container > input[type="text"]').val('');
     };
 
-    $('.order-call').on('click', function(){
-        showPopup();
-    });
-
     $('.close').on('click', function(){
         clearForm();
         closePopup();
